8f4bdaed05dc65953102c13497bbcf1c767884fd
ninaweng6716 <146882360+ninaweng6716@users.noreply.github.com>
2024-05-12 00:11:08 -0500

    added comments

--- src/components/MovieCardGrid.jsx
+++ src/components/MovieCardGrid.jsx
@@ -5,6 +5,7 @@ import { useDispatch } from 'react-redux';
 import { addFav, deleteFav } from '../features/favs/favsSlice';
 import {FavButton} from './FavButton';
 
+// basically a lot of repetition from MovieCard.jsx...could probably optimize it lol
 function MovieCardGrid({movie, isFav}) {
     
         const dispatch = useDispatch();
@@ -18,12 +19,13 @@ function MovieCardGrid({movie, isFav}) {
             }
         }
 
-        // parse movie.overview to 100 characters
+        // parse movie.overview to 100 characters as grid cards are smaller
         if (movie.overview.length > 100) {
             movie.overview = movie.overview.substring(0, 100) + "...";
         }
     
         return (
+            // basically all the same as MovieCard.jsx without the stylel={{}} stuff
             <div className="relative shrink-0 cursor-pointer rounded-2xl bg-white shadow-md transition-all hover:scale-[1.015] hover:shadow-xl">
                 <img className="rounded-t-2xl" src={`${imageBaseURL}w500${movie.poster_path}`} alt={movie.title} />
                 <div className="absolute inset-0 z-20 rounded-2xl bg-gradient-to-b from-black/90 via-black/60 to-black/0 p-6 text-white transition-[backdrop-filter] hover:backdrop-blur-sm">
--- src/pages/PageMovies.jsx
+++ src/pages/PageMovies.jsx
@@ -7,7 +7,9 @@ import MovieCardGrid from '../components/MovieCardGrid';
 function PageMovies() {
     const [movies, setMovies] = useState([]);
     const [genre, setGenre] = useState([]);
-    const [selectedGenre, setSelectedGenre] = useState(35); // Default to comedy genre
+    // default movie genre is action
+    const [selectedGenre, setSelectedGenre] = useState(28);
+
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
 
@@ -15,6 +17,8 @@ function PageMovies() {
         document.title = `Browse Movies by Genre | ${appTitle}`;
     }, []);
 
+    // fetch genre list using api key – each genre has an associated id and name
+    // if no errors, set genre from the fetched data
     useEffect(() => {
         const fetchGenre = async () => {
             setLoading(true);
@@ -33,12 +37,18 @@ function PageMovies() {
         fetchGenre();
     }, []);
 
+    // fetching movies by genre using selected genre id
     useEffect(() => {
         const fetchMoviesByGenre = async () => {
             setLoading(true);
             try {
+                // this is how you build the url for fetching movies by genre: https://api.themoviedb.org/3/discover/movie?api_key=69bba2f5467e7c956ecaaa7af1cc21a6&with_genres=28
+                // where 28 is the id for action movies
                 const response = await fetch(`${movieGenreBaseURL}?api_key=${apiKey}${movieGenreBaseURLAfterAPI}${selectedGenre}`);
+
                 const data = await response.json();
+
+                // if there are results, set movies from the fetched data
                 if (data.results && data.results.length > 0) {
                     setMovies(data.results);
                 } else {
@@ -51,9 +61,11 @@ function PageMovies() {
             }
         };
 
+        // fetch movies by genre only if a genre is selected – hence the selectedGenre in dependency array
         fetchMoviesByGenre();
     }, [selectedGenre]);
 
+    // a simple function to handle genre change
     const handleChangeGenre = (genre) => {
         setSelectedGenre(genre);
     };
@@ -75,10 +87,12 @@ function PageMovies() {
                         ))}
                     </select>
                 </div>
+
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 m-4">
                     {loading && <div>Loading...</div>}
                     {error && <div>{error}</div>}
                     {movies.map((movie) => (
+                        // created a moviecardgrid component to display movie cards in grid
                         <MovieCardGrid key={movie.id} movie={movie} />
                     ))}
                 </div>
