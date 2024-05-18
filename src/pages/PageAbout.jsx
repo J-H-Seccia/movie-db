7bc5472fc21cd1ada121d33ef5ac428bcf2e9e5b
cchan395 <156140027+cchan395@users.noreply.github.com>
2024-05-18 11:26:18 -0700

    added about us text

--- src/pages/PageAbout.jsx
+++ src/pages/PageAbout.jsx
@@ -8,9 +8,23 @@ function PageAbout() {
     }, []);
 
     return ( 
-        <h1 className="text-3xl font-bold underline">
-            About
-        </h1>
+        <>
+            <h1 className="text-3xl font-bold underline">
+                About
+            </h1>
+            <p>At ScreenScape, we're passionate about films. Whether you're a casual moviegoer, a dedicated cinephile, or a filmmaker yourself, our platform is designed to cater to your every need.
+            </p>
+            <p>Explore our vast collection of movies spanning across genres, decades, and cultures. From classic masterpieces to the latest blockbusters, we've got you covered.
+            </p>
+            <p>But ScreenScape is more than just a repository of films. It's a community of fellow movie enthusiasts, where you can connect, discuss, and share your love for cinema.
+            </p>
+            <p>Our mission is to make the magic of movies accessible to everyone. Whether you're looking for recommendations, trivia, or behind-the-scenes insights, you'll find it here.
+            </p>
+            <p>Join us on this cinematic journey and let's celebrate the art of storytelling together.
+            </p>
+            <p>ScreenScape - Where Every Movie Has a Story.
+            </p>
+        </>
      );
 }
 
