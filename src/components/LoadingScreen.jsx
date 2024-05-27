b57b88e77ef9e1e35f87f3d7a4c48fe07c390705
ninaweng6716 <146882360+ninaweng6716@users.noreply.github.com>
2024-05-27 12:30:11 -0700

    fixed margin on loading screen

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
 function LoadingScreen() {
+
     return (
-        <div className="fixed top-0 left-0 z-50 w-screen h-screen flex items-center justify-center bg-bg-pattern">
-            <div className="text-white text-2xl font-bold">Loading movies...we should be done soon</div>
-        </div>
-    );
+        <>
+            <div className="fixed top-0 left-0 z-50 w-screen h-screen flex items-center justify-center bg-bg-pattern">
+                <div className="flex flex-col m-8 text-white text-2xl font-bold">Loading movies...we should be done soon</div>
+            </div>
+        </>
+    )
+   
 }
 
 export default LoadingScreen;
